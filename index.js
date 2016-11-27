d3fbd6333b5a77acb5e75463802fdfa94e0c580b
Hilmar Lapp <hlapp@drycafe.net>
2016-11-27 12:45:40 -0500

    Moves platform object into submodule
    Also takes the first step to moving the API calling logic into the
    platform submodule, where I think it belongs (platform API calling
    is part of the platform business). This unfortunately makes for
    circular require()s, hence moving module.exports up to come prior to
    the require() calls.

--- index.js
+++ index.js
@@ -3,77 +3,9 @@
  */
 "use strict";
 
-var request = require('request'),
-    util = require('util'),
-    EventEmitter = require('events'),
-    lib = require('./lib/util.js'),
-    WirelessTagManager = require('./lib/tagmanager.js');
-
 /*
  * The cloud platform interface to the Wireless Tag platform
  */
-function WirelessTagPlatform(config, log) {
-    EventEmitter.call(this);
-    this.config = config;
-    this.log = log || console.log;
-    this.tagManagers = [];
-    this.tagManagerMap = {};
-    this.errorHandler = config.errorHandler || lib.defaultHandler;
-}
-util.inherits(WirelessTagPlatform, EventEmitter);
-
-
-// Authenticates against the wireless tag cloud.
-WirelessTagPlatform.prototype.connect = function(config, log, callback) {
-    this.config = config || this.config;
-    this.log = log || this.log;
-
-    this.isConnected().then(
-        (connected) => {
-            if (connected) return this;
-            return lib.doAPIrequest(
-                '/ethAccount.asmx/Signin',
-                'POST',
-                { email: this.config.username, password: this.config.password },
-                callback)
-                .then(
-                    (res) => { this.emit('connect', this); },
-                    this.errorHandler(callback)
-                );
-        },
-        this.errorHandler(callback)
-    );
-    return this;
-}
-
-WirelessTagPlatform.prototype.isConnected = function(callback) {
-    return lib.doAPIrequest('/ethAccount.asmx/IsSignedIn',
-                            'POST',
-                            {},
-                            callback);
-}
-
-WirelessTagPlatform.prototype.discoverTagManagers = function(query, callback) {
-    var filter = lib.createFilter(query);
-    var req = lib.doAPIrequest(
-        '/ethAccount.asmx/GetTagManagers',
-        'POST',
-        {},
-        callback);
-    return req.then(
-        (result) => {
-            result = result.filter(filter);
-            this.tagManagers = [];
-            this.tagManagerMap = {};
-            for (let mgrData of result) {
-                let tagManager = new WirelessTagManager(this, mgrData);
-                this.tagManagers.push(tagManager);
-                this.tagManagerMap[tagManager.mac] = tagManager;
-                this.emit('discover', tagManager);
-            };
-        },
-        this.errorHandler(callback)
-    );
-}
+var WirelessTagPlatform = require('./lib/platform.js');
 
 module.exports = WirelessTagPlatform;

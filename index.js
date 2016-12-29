ea2bb0fe0f1c1e538d4efa488e772b0aad0fa5d7
Hilmar Lapp <hlapp@drycafe.net>
2016-12-29 17:23:39 -0500

    First start at JSDoc documentation
    This is another step towards addressing #10.

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
  */
 "use strict";
 
-/*
- * The cloud platform interface to the Wireless Tag platform
+/**
+ * The cloud platform interface to the Wireless Tag platform. This
+ * module exports the constructor for the {@link WirelessTagPlatform}
+ * class.
+ *
+ * @module wirelesstags
  */
 var WirelessTagPlatform = require('./lib/platform');
 
@@ -12,11 +16,45 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
+/**
+ * @const {string} - The name of the default configuration file in the 
+ *                   executing user's home directory.
+ *                   Used by {@link WirelessTagPlatform.loadConfig}.
+ * @default
+ */
 const CONFIG_NAME = ".wirelesstags";
+
+/**
+ * @const {string} - The environment variable containing the Wireless
+ *                   Tag API user name.
+ *                   Used by {@link WirelessTagPlatform.loadConfig}.
+ * @default
+ */
 const ENV_USERNAME = "WIRELESSTAG_API_USER";
+
+/**
+ * @const {string} - The environment variable containing the Wireless
+ *                   Tag API password.
+ *                   Used by {@link WirelessTagPlatform.loadConfig}.
+ * @default
+ */
 const ENV_PASSWORD = "WIRELESSTAG_API_PASSWORD";
+
 const ENV_TOKEN = "WIRELESSTAG_API_TOKEN";
 
+/**
+ * Loads config information, which presently consists primarily of
+ * connection options.
+ *
+ * The algorithm will attempt to read the file CONFIG_NAME (in JSON
+ * format) in the executing user's home directory if the file
+ * exists. It will then take username and password from the
+ * environment (ENV_USERNAME and ENV_PASSWORD), which allows to use
+ * the environment to override settings in the configuration file.
+ *
+ * @returns {Object}
+ * @memberof WirelessTagPlatform
+ */
 WirelessTagPlatform.loadConfig = function() {
     let config = {};
     let confPath = path.join(os.homedir ? os.homedir() : process.env.HOME,
@@ -40,6 +78,23 @@ WirelessTagPlatform.loadConfig = function() {
     return config;
 };
 
+/**
+ * Creates a {@link WirelessTagPlatform} instance, using the given
+ * options for initializing. In contrast to the constructor, this
+ * method will first attempt to load default configuration using
+ * {@link WirelessTagPlatform.loadConfig}.
+ *
+ * @param {Object} [options] - overrides options found in the default
+ *   configuration loaded using {@link WirelessTagPlatform.loadConfig}.
+ * @param {String} [options.log] - a custom log function.
+ * @param {String} [options.errorHandler] - a function returning a custom
+ *                       error handler, will be passed a callback function.
+ * @param {String} [options.apiBaseURI] - the base URI of the API server
+ *                        if hosted on a different server than the default
+ *
+ * @returns {WirelessTagPlatform}
+ * @memberof WirelessTagPlatform
+ */
 WirelessTagPlatform.create = function(options) {
     let config = WirelessTagPlatform.loadConfig();
     for (let key in options) {

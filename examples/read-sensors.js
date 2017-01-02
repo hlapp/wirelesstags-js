f3ca40fa234fcf2a616b3f85867e5d9e35916ae4
Hilmar Lapp <hlapp@drycafe.net>
2017-01-01 19:36:33 -0500

    Mostly cosmetic fixes to the example doc.

--- examples/read-sensors.js
+++ examples/read-sensors.js
@@ -1,27 +1,35 @@
 "use strict";
 
 /** 
- * Simple example script that demonstrates what might be a typical flow:
+ * Simple example script that demonstrates what might be a
+ * typical flow for reading sensors.
  * 
+ * The principle flow is the following:
+ *
  * 1. Connect to cloud.
  * 2. Find tag managers.
  * 3. For each tag manager, find its associated tags.
  * 4. For each tag, find its sensors.
- * 5. For each sensor, log useful properties (here: reading, event
- *    state (such as "Too High" for temperature), and whether it is
- *    armed (meaning that events trigger alerts, such as push notification).
+ * 5. For each tag and its sensors, log useful properties (for
+ *    sensors, reading, event state (such as "Too High" for
+ *    temperature), and whether it is armed (meaning that events
+ *    trigger alerts, such as push notification).
  * 6. Keep looping for a set period of time by updating the data for
- *    each tag at the interval configured for each tag.
+ *    each tag at the interval configured for each tag. Log sensor
+ *    properties every time data for the respective tag is updated.
  * 
  * The implementation here uses mostly event handlers to proceed once
  * the initial connect() succeeds. One could equally well use
- * Promise-chaining (via .then()).
+ * Promise-chaining (via `.then()`).
  *
  * Both tag and sensor objects override the .toString() method to
  * produce a compact stringified-JSON representation of their key
  * properties, so dumping this to the terminal can be informative.
  * Uncomment the corresponding lines in the code below to do so.
+ *
+ * @module
  */
+
 var Platform = require('../'),
     platform = Platform.create();
 
@@ -52,11 +60,13 @@ platform.on('discover', (manager) => {
 platform.connect(Platform.loadConfig()).
     catch((e) => { console.error(e.stack ? e.stack : e) });
 
+/** Logs properties of the given tag. */
 function logTag(tag) {
     console.log("Tag", tag.name, "(slaveId=" + tag.slaveId + ")");
     // console.log(".. properties:", tag.toString());
 }
 
+/** Logs properties of the given sensor. */
 function logSensor(sensor) {
     console.log("..", sensor.sensorType,
                 "of", sensor.wirelessTag.name + ":");
